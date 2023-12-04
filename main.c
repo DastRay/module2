45834b6b2020dc904b390ba67de9664134f97979
DastRay <104367556+DastRay@users.noreply.github.com>
2023-12-04 12:51:54 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
-
 
 enum {
    max_name_len = 30,
    max_post_len = 20,
    max_date_len = 12,
+   max_sex_len = 10,
    max_empl_num = 100
 };
 
@@ -15,8 +14,8 @@ typedef struct {
    char name[max_name_len];
    char ptrnmc[max_name_len]; // patronymic
    char post[max_post_len];
-   char sex[10];
-   char hire_date[max_date_len];
+   char sex[max_sex_len];
+   char hiredate[max_date_len];
 } Employees_Data;
 
 typedef struct {
@@ -24,7 +23,7 @@ typedef struct {
    int num_empl;
 } Employees_storage;
 
-//С„СѓРЅРєС†РёСЏ РґР»СЏ РґРѕР±Р°РІР»РµРЅРёСЏ РЅРѕРІРіРѕ СЃРѕС‚СЂСѓРґРЅРёРєР° РІ С…СЂР°РЅРёР»РёС‰Рµ Employees_storage
+//функция для добавления новго сотрудника в хранилище Employees_storage
 void addEmployee(Employees_storage* storage_, const char* surname_, const char* name_, const char* ptrnmc_, const char* post_, \
    const char* sex_,  const char* hire_date_)
 {
@@ -36,7 +35,7 @@ void addEmployee(Employees_storage* storage_, const char* surname_, const char* 
       strcpy_s(new_employee.ptrnmc, sizeof(new_employee.ptrnmc), ptrnmc_);
       strcpy_s(new_employee.post, sizeof(new_employee.post), post_);
       strcpy_s(new_employee.sex, sizeof(new_employee.sex), sex_);
-      strcpy_s(new_employee.hire_date, sizeof(new_employee.hire_date), hire_date_);
+      strcpy_s(new_employee.hiredate, sizeof(new_employee.hiredate), hire_date_);
 
       storage_->employee[storage_->num_empl] = new_employee;
       storage_->num_empl++;
@@ -44,25 +43,25 @@ void addEmployee(Employees_storage* storage_, const char* surname_, const char* 
 
    else
    {
-      printf("РҐСЂР°РЅРёР»РёС‰Рµ Р·Р°РїРѕР»РЅРµРЅРѕ! Р”РѕР±Р°РІРёС‚СЊ РЅРѕРІРѕРіРѕ СЃРѕС‚СЂСѓРґРЅРёРєР° РЅРµРІРѕР·РјРѕР¶РЅРѕ");
+      printf("Хранилище заполнено! Добавить нового сотрудника невозможно");
    }
 }
 
-// С„СѓРЅРєС†РёСЏ РІС‹РІРѕРґР° РёРЅС„РѕСЂРјР°С†РёРё Рѕ СЃРѕС‚СЂСѓРґРЅРёРєР°С… РёР· С…СЂР°РЅРёР»РёС‰Р° Employees_storage
+// функция вывода информации о сотрудниках из хранилища Employees_storage
 void printStorage(const Employees_storage* storage_)
 {
-   printf("РЎРІРµРґРµРЅРёСЏ Рѕ СЃРѕС‚СЂСѓРґРЅРёРєР°С…:\n-------------------------------------\n");
+   printf("Сведения о сотрудниках:\n-------------------------------------\n");
 
    for (int i = 0; i < storage_->num_empl; i++)
    {
-      printf("РЎРѕС‚СЂСѓРґРЅРёРє в„– %d\n", i + 1);
-      printf("Р¤Р°РјРёР»РёСЏ: %s\n", storage_->employee[i].surname);
-      printf("РРјСЏ: %s\n", storage_->employee[i].name);
-      printf("РћС‚С‡РµСЃС‚РІРѕ: %s\n", storage_->employee[i].ptrnmc);
-      printf("Р”РѕР»Р¶РЅРѕСЃС‚СЊ: %s\n", storage_->employee[i].post);
-      printf("РџРѕР»: %s\n", storage_->employee[i].sex);
-      printf("Р”Р°С‚Р° РїСЂРёС‘РјР° РЅР° СЂР°Р±РѕС‚Сѓ: %s\n", storage_->employee[i].hire_date);
-      printf("-------------------------------------\n");
+      printf("Сотрудник №%d\n", i + 1);
+      printf("Фамилия: %s", storage_->employee[i].surname);
+      printf("Имя: %s", storage_->employee[i].name);
+      printf("Отчество: %s", storage_->employee[i].ptrnmc);
+      printf("Должность: %s", storage_->employee[i].post);
+      printf("Пол: %s", storage_->employee[i].sex);
+      printf("Дата приёма на работу: %s", storage_->employee[i].hiredate);
+      printf("\n-------------------------------------\n");
    }
 }
 
@@ -70,14 +69,56 @@ int main()
 {
    setlocale(LC_ALL, "Rus");
 
+   FILE* file;
+   errno_t err = fopen_s(&file, "file.txt", "r");
+
+   if (err != 0 || !file)
+   {
+      perror("File cant't open!");
+      return 1;
+   }
+
    Employees_storage employees;
    employees.num_empl = 0;
 
-   addEmployee(&employees, "РРІР°РЅРѕРІ", "РРІР°РЅ", "РРІР°РЅРѕРІРёС‡", "Р”РёСЂРµРєС‚РѕСЂ", "РњСѓР¶СЃРєРѕР№", "01-01-2000");
-   addEmployee(&employees, "РђР»РµРєСЃР°РЅРґСЂРѕРІ", "РђР»РµРєСЃР°РЅРґСЂ", "РђР»РµРєСЃР°РЅРґСЂРѕРІРёС‡", "РњРµРЅРµРґР¶РµСЂ", "РњСѓР¶СЃРєРѕР№", "02-01-2000");
-   addEmployee(&employees, "РР»СЊРёРЅ", "РђР»РµРєСЃР°РЅРґСЂ", "Р’Р°Р»РµСЂСЊРµРІРёС‡", "Р‘СѓС…РіР°Р»С‚РµСЂ", "РњСѓР¶СЃРєРѕР№", "31-01-2020");
-   addEmployee(&employees, "РР»СЊРёРЅ", "РђР»РµРєСЃР°РЅРґСЂ", "Р’Р°Р»РµСЂСЊРµРІРёС‡", "Р‘СѓС…РіР°Р»С‚РµСЂ", "РњСѓР¶СЃРєРѕР№", "31-01-2020");
-   addEmployee(&employees, "РЎРёРґРѕСЂРѕРІР°", "Р•Р»РµРЅР°", "Р’РёРєС‚РѕСЂРѕРІРЅР°", "Р‘СѓС…РіР°Р»С‚РµСЂ", "Р–РµРЅСЃРєРёР№", "10-10-2012");
+   char sur_name[max_name_len];
+   char na_me[max_name_len];
+   char ptr_nmc[max_name_len];
+   char po_st[max_post_len];
+   char s_ex[max_sex_len];
+   char hire_date[max_date_len];
+
+   printf("Данные считываются.\n");
+   int count = 0;
+   while (count < max_empl_num)
+   {
+      if (feof(file))
+      {
+         printf("Все данные успешно считаны считаны.\n");
+         printf("\n");
+         break;
+      }
+
+      else
+      {
+         fgets(sur_name, max_name_len, file);
+         fgets(na_me, max_name_len, file);
+         fgets(ptr_nmc, max_name_len, file);
+         fgets(po_st, max_post_len, file);
+         fgets(s_ex, max_sex_len, file);
+         fgets(hire_date, max_date_len, file);
+         
+
+         addEmployee(&employees, sur_name, na_me, ptr_nmc, po_st, s_ex, hire_date);
+         count++;
+      }
+   }
+  
+   /*addEmployee(&employees, "Иванов", "Иван", "Иванович", "Директор", "Мужской", "01-01-2000");
+   addEmployee(&employees, "Александров", "Александр", "Александрович", "Менеджер", "Мужской", "02-01-2000");
+   addEmployee(&employees, "Ильин", "Александр", "Валерьевич", "Бухгалтер", "Мужской", "31-01-2020");
+   addEmployee(&employees, "Ильин", "Александр", "Валерьевич", "Бухгалтер", "Мужской", "31-01-2020");
+   addEmployee(&employees, "Сидорова", "Елена", "Викторовна", "Бухгалтер", "Женский", "10-10-2012");*/
 
    printStorage(&employees);
 
