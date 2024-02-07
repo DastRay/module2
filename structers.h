98ab28e40217dbeb9909fac849cd37b0e82c0f64
DastRay <104367556+DastRay@users.noreply.github.com>
2024-02-07 20:30:59 +0300

    Add files via upload

--- structers.h
+++ structers.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdbool.h>
+
 enum {
    MAX_NAME_LEN = 30,
    MAX_POST_LEN = 20,
@@ -20,7 +22,20 @@ typedef struct {
 typedef struct {
    Employee employee[MAX_EMPL_NUM];
    int num_empl;
+   bool eror;
 } Employees_storage;
+
+/**
+ * @brief функция считывани¤ информации из файла
+ * @param file файл, из котрого считываем
+*/
+void readFile(FILE* file);
+
+/**
+* @brief  функци¤ вывода информации о сотрудниках из хранилища Employees_storage
+* @param storage_ указатель на хранилище струткур
+*/
+void printStorage(const Employees_storage* storage_);
 
 /**
  * @brief функция для добавления новго сотрудника в хранилище Employees_storage
@@ -30,27 +45,35 @@ typedef struct {
  * @param ptrnmc_ отчество
  * @param post_ должность
  * @param sex_ пол
- * @param hire_date_d_ день приёма на работу
- * @param hire_date_m_ месяц приёма на работу
- * @param hire_date_y_ год приЄма на работу
+ * @param hire_date_d_ день приЄма на работу
+ * @param hire_date_m_ мес¤ц приЄма на работу
+ * @param hire_date_y_ год примЄа на работу
 */
 void addEmployee(Employees_storage* storage_, const char* surname_, const char* name_, const char* ptrnmc_, const char* post_, \
    const char* sex_, int hire_date_d_, int hire_date_m_, int hire_date_y_);
 
 /**
-* @brief  функция вывода информации о сотрудниках из хранилища Employees_storage
-* @param storage_ указатель на хранилище струткур
+ * @brief добовление сотрудника в файл и хранилище
+ * @param storage_ хранилище куда добавляем
+ * @param file файл куда добавляем
 */
-void printStorage(const Employees_storage* storage_);
+void addEmployeeFile(const Employees_storage* storage_, FILE* file);
 
 /**
- * @brief функция выводящяя информацию о сотрудниках, стаж которых превышает 10 лет
+ * @brief удаление сотрудника из файла
+ * @param storage_ указатель на хранилище структур
+ * @param file файл из которого удаляется сотрудник
+*/
+void deleteEmployeeFile(const Employees_storage* storage_, FILE* file);
+
+/**
+ * @brief функция выводящяя информацию о сотрудниках, стаж которых превышает n лет
  * @param storage_ указатель на хранилище структур
 */
 void checkExp(const Employees_storage* storage_);
 
 /**
- * @brief функция считывания информации из файла
- * @param file файл, из котрого считываем
+* @brief вызов меню дл¤ работы с данными о сотрудниках
+* @param storage_ хранилище в котором наход¤тс¤ данные о сотрудниках
 */
-void readFile(FILE* file);
+void MyMenu(const Employees_storage* storage_, FILE* file);
